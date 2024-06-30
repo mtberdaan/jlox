7fbf79b390ec6d513384fc8427bbf00889b6f573
Daan <daandenhoed@gmail.com>
2024-06-30 10:55:00 +0200

    format 4space

--- com/mtberdaan/tool/GenerateAst.java
+++ com/mtberdaan/tool/GenerateAst.java
@@ -6,61 +6,61 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-  public static void main(String[] args) throws IOException {
-    if (args.length != 1) {
-      System.err.println("Usage: generate_ast <output directory>");
-      System.exit(64);
-    }
-    String outputDir = args[0];
-    defineAst(outputDir, "Expr", Arrays.asList(
-      "Binary : Expr left, Token operator, Expr right",
-        "Grouping : Expr expression", 
-        "Literal : Object value", 
-        "Unary : Token operator, Expr right"));
-  }
-
-  private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
-    String path = outputDir + "/" + baseName + ".java";
-    PrintWriter writer = new PrintWriter(path, "UTF-8");
-
-    writer.println("package com.mtberdaan.lox;");
-    writer.println();
-    writer.println("import java.util.List;");
-    writer.println();
-    writer.println("abstract class " + baseName + " {");
-
-    // The AST classes
-    for (String type : types) {
-      String className = type.split(":")[0].trim();
-      String fields = type.split(":")[1].trim();
-      defineType(writer, baseName, className, fields);
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression", 
+            "Literal : Object value", 
+            "Unary : Token operator, Expr right"));
     }
 
-    writer.println("}");
-    writer.close();
-  }
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-  private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
-    writer.println("  static class " + className + " extends " + baseName + " {");
+        writer.println("package com.mtberdaan.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
 
-    // Constructor
-    writer.println("    " + className + "(" + fieldList + ") {");
+        // The AST classes
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
 
-    // Store parameters in fields
-    String[] fields = fieldList.split(", ");
-    for (String field : fields) {
-      String name = field.split(" ")[1];
-      writer.println("      this." + name + " = " + name + ";");
+        writer.println("}");
+        writer.close();
     }
 
-    writer.println("    }");
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
 
-    // Fields
-    writer.println();
-    for (String field : fields) {
-      writer.println("    final " + field + ";");
+        // Constructor
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // Fields
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
     }
-
-    writer.println("  }");
-  }
 }
